0cba779f0b47378117cd779254e33ae43ad8334c
sufeiiz <szhao2@gmail.com>
2015-06-30 20:17:13 -0400

    add

--- UpdateMe/app/src/main/java/nyc/c4q/syd/updateme/MainActivity.java
+++ UpdateMe/app/src/main/java/nyc/c4q/syd/updateme/MainActivity.java
@@ -1,39 +1,6 @@
 package nyc.c4q.syd.updateme;
 
 import android.app.Activity;
-<<<<<<< HEAD
-import android.content.Intent;
-import android.os.AsyncTask;
-=======
->>>>>>> recovery
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ProgressBar;
-<<<<<<< HEAD
-import android.widget.TextView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.json.JSONArray;
-=======
-import java.util.ArrayList;
-import java.util.List;
->>>>>>> recovery
 
 
 /**
